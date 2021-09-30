4a2d2617ab9107b180bff1c6fd843703019333b1
Lauren Durr <laurenashdurr@gmail.com>
2021-09-30 19:15:37 -0400

    updated DB options

--- db.js
+++ db.js
@@ -2,7 +2,12 @@ const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres', 
-    })
+        dialectOptions: {
+            ssl:{
+                    require: true,
+                    rejectUnauthorized: false
+                }
+    }})
 
 async function syncDb(sequelize, options) {
     const { force, alter } = options
